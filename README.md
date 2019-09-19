9960111826814beeeb047707f16af1e030b6d7a1
seanaleid <seanaleid@gmail.com>
2019-09-19 16:39:06 -0400

    Step 2 completed: validation included for name, email, and password

--- README.md
+++ README.md
@@ -13,15 +13,15 @@ We've seen many different styles of form management by now -- simple to complex.
   - `axios`
 - [X] Create a component file called `Form.js`, import it into your `App.js` file, and place the component in your JSX there.
 
-## STEP 1 - Create Your Formik Form
+[X]## STEP 1 - Create Your Formik Form
 
 We want to create a form to onboard a new user to our system. We need _at least_ the following pieces of information about our new user:
 
-- Name
-- Email
-- Password
-- Terms of Service (checkbox)
-- A Submit button to send our form data to the server.
+-[X] Name
+-[X] Email
+-[X] Password
+-[X] Terms of Service (checkbox)
+-[X] A Submit button to send our form data to the server.
 
 ## STEP 2 - Implement Form Validation and Error Messaging
 
--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,18 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {withFormik, Form, Field} from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
-const UserForm = ({values}) => {
+const UserForm = ({values, errors, touched}) => {
+    const [user, setUser] = useState([]);
+    //set useState outside of the return, need to include the useState to handle the data being posted
+
 
     return(
         <div className="user-form">
             <Form>
                 <Field type="text" name="name" placeholder="Name" />
+                {touched.name && errors.name && (
+                    <p className="error">{errors.name}</p>
+                )}
                 <Field type="email" name="email" placeholder="Email" />
+                {touched.email && errors.email && (
+                    <p className="error">{errors.email}</p>
+                )}
                 <Field type="password" name="password" placeholder="Password" />
+                {touched.password && errors.password && (
+                    <p className="error">{errors.password}</p>
+                )}
                 <label className="checkbox-container">
                     <Field type="checkbox" name="terms" checked={values.terms} />
+                    {touched.terms && errors.terms && (
+                    <p className="error">{errors.terms}</p>
+                )}
                     <span className="checkmark" />
                 </label>
                 <button>Submit!</button>
@@ -22,11 +37,19 @@ const UserForm = ({values}) => {
 };
 
 const FormikUserForm = withFormik({
-    mapPropsToValues({terms}){
+    mapPropsToValues({name, email, password, terms}){
         return{
+            name: name || "",
+            email: email || "",
+            password: password || "",
             terms: terms || false
         };
     },
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required(`You must include a valid name`),
+        email: Yup.string().email().required(`You must include a valid email`),
+        password: Yup.string().min(9).required(`You must include a password of at least 9 characters`),
+    }),
 })(UserForm);
 
 
