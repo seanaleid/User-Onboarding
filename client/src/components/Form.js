98ea151ce0a7ae5157ffabb9b1b734bf812d4340
seanaleid <seanaleid@gmail.com>
2019-09-19 19:01:58 -0400

    2nd stretch, added more inputs

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -28,9 +28,25 @@ const UserForm = ({values, errors, touched, status}) => {
                 {touched.email && errors.email && (
                     <p className="error">{errors.email}</p>
                 )}
-                <Field type="address" name="address" placeholder="Address" />
+                <Field type="text" name="address" placeholder="Address" />
                 {touched.address && errors.address && (
                     <p className="error">{errors.address}</p>
+                )}
+                <Field type="text" name="city" placeholder="City" />
+                {touched.city && errors.city && (
+                    <p className="error">{errors.city}</p>
+                )}
+                <Field type="text" name="zipcode" placeholder="Zipcode" />
+                {touched.zipcode && errors.zipcode && (
+                    <p className="error">{errors.zipcode}</p>
+                )}
+                <Field type="text" name="phone" placeholder="Phone Number" />
+                {touched.phone && errors.phone && (
+                    <p className="error">{errors.phone}</p>
+                )}
+                <Field type="text" name="username" placeholder="Username" />
+                {touched.username && errors.username && (
+                    <p className="error">{errors.username}</p>
                 )}
                 <Field type="password" name="password" placeholder="Password" />
                 {touched.password && errors.password && (
@@ -51,6 +67,10 @@ const UserForm = ({values, errors, touched, status}) => {
                     <li>Last Name: {user.lastName}</li>
                     <li>Email: {user.email}</li>
                     <li>Address: {user.address}</li>
+                    <li>City: {user.city}</li>
+                    <li>Zipcode: {user.zipcode}</li>
+                    <li>Phone: {user.phone}</li>
+                    <li>Username: {user.username}</li>
                     <li>Password: {user.password}</li>
                 </ul>
             ))}
@@ -59,12 +79,16 @@ const UserForm = ({values, errors, touched, status}) => {
 };
 
 const FormikUserForm = withFormik({
-    mapPropsToValues({firstName, lastName, email, address, password, terms}){
+    mapPropsToValues({firstName, lastName, email, address, city, zipcode, phone, username, password, terms}){
         return{
             firstName: firstName || "",
             lastName: lastName || "",
             email: email || "",
             address: address || "",
+            city: city || "",
+            zipcode: zipcode || "",
+            phone: phone || "",
+            username: username || "",
             password: password || "",
             terms: terms || false
         };
@@ -74,6 +98,10 @@ const FormikUserForm = withFormik({
         lastName: Yup.string().required(`You must include a valid name`),
         email: Yup.string().email().required(`You must include a valid email`),
         address: Yup.string().required(`You must include a valid address`),
+        city: Yup.string().required(`You must include a valid address`),
+        zipcode: Yup.string().required(`You must include a valid address`),
+        phone: Yup.string().required(`You must include a valid address`),
+        username: Yup.string().required(`You must include a valid address`),
         password: Yup.string().min(9).required(`You must include a password of at least 9 characters`),
     }),
     handleSubmit(values, {setStatus}){
