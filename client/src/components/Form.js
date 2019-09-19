b4d989140658c228fc9ba4bc35230ab1754469e6
seanaleid <seanaleid@gmail.com>
2019-09-19 17:01:32 -0400

    Step 3 completed: the axios.post is function.

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -6,7 +6,12 @@ import axios from "axios";
 const UserForm = ({values, errors, touched}) => {
     const [user, setUser] = useState([]);
     //set useState outside of the return, need to include the useState to handle the data being posted
-
+    useEffect((status)=>{
+        if(status){
+            setUser([...user, status]);
+        }
+    }, [user]);
+    //the dependency array is watching for changes in status
 
     return(
         <div className="user-form">
@@ -50,6 +55,15 @@ const FormikUserForm = withFormik({
         email: Yup.string().email().required(`You must include a valid email`),
         password: Yup.string().min(9).required(`You must include a password of at least 9 characters`),
     }),
+    handleSubmit(values, {setStatus}){
+        axios.post(`https://reqres.in/api/users/`, values)
+        .then(response => {
+            setStatus(response.data);
+            //setStatus comes from Formik, the data is what the user submits. 
+            console.log(response.data);
+            //ran a few tests, the data is being posted
+        })
+    }
 })(UserForm);
 
 
